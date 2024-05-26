4c439320dbf57a6d4b4991db31581a3bc8983314
Your Name <you@example.com>
2024-05-26 17:08:59 +0530

    stopwatch_up

--- stopwatch_buildout/src/Stopwattch.jsx
+++ stopwatch_buildout/src/Stopwattch.jsx
@@ -1,52 +1,106 @@
-import React,{useState} from "react";
+// import React,{useState} from "react";
 
-const Stopwatch = () =>{
-const [currenttime , setcurrenttime] = useState(0);
-const [toggle, settoggle] = useState("Start");
-const [interval,setinterval] = useState(null);
+// const Stopwatch = () =>{
+// const [currenttime , setcurrenttime] = useState(0);
+// const [toggle, settoggle] = useState("Start");
+// const [intervalId,setintervalId] = useState(null);
 
-const handle = ()=>{
-    if(toggle === "Start"){
-        settoggle("Stop");
+// const handle = ()=>{
+//     if(toggle === "Start"){
+//         settoggle("Stop");
 
-        const timer  = setInterval(()=>{
-            setcurrenttime((prev)=>prev+1);
-        },1000);
-        setinterval(timer);
-    }else{
-        settoggle("Start");
-        clearInterval(interval);
-        setinterval(null);
+//         const timer  = setInterval(()=>{
+//             setcurrenttime((prev)=>prev+1);
+//         },1000);
+//         setintervalId(timer);
+//     }else{
+//         settoggle("Start");
+//         clearInterval(intervalId);
+//         setintervalId(null);
+//     }
+// };
+
+// const handlereset = () =>{
+//     setcurrenttime(0);
+//     clearInterval(intervalId);
+//     setintervalId(null);
+//     settoggle("Start");
+// };
+
+// const format = () => {
+//     let min = Math.floor(currenttime / 60);
+//     let sec = currenttime % 60;
+//     if (sec < 10) sec = "0" + sec;
+//     return `${min}:${sec}`;
+//   };
+
+// return(
+//     <>
+//     <h1>STOPWATCH</h1>
+//     <div>Time: {format()}</div>
+//     <div>
+//         <button className="button" onClick={handle}>{toggle}</button>
+//         <button className="button" onClick={handlereset}>Reset</button>
+//     </div>
+//     </>
+// )
+
+
+// }
+
+
+// export default Stopwatch;
+
+import React, { useState } from "react";
+
+const Stopwatch= () => {
+  const [curTime, setCurTime] = useState(0);
+  const [toggle, setToggle] = useState("Start");
+  const [intervalId, setIntervalId] = useState(null);
+
+  const handleWatch = () => {
+    if (toggle === "Start") {
+      setToggle("Stop");
+      const timer = setInterval(() => {
+        setCurTime((prev) => prev + 1);
+      }, 1000);
+      setIntervalId(timer);
+    } else {
+      setToggle("Start");
+      clearInterval(intervalId);
+      setIntervalId(null);
     }
-};
+  };
 
-const handlereset = () =>{
-    setcurrenttime(0);
-    clearInterval(interval);
-    setinterval(null);
-    settoggle("Start");
-};
+  const handleReset = () => {
+    setCurTime(0);
+    clearInterval(intervalId);
+    setIntervalId(null);
+    setToggle("Start");
+  };
 
-const format =() =>{
-    let min  = Math.floor(Number(currenttime)/60);
-    let sec = Number(currenttime) % 60;
-    if(String(sec).length <2) sec = "0"+String(sec);
-    return `${min}:${sec}`;
-}
+  const formatted = () => {
+    let minutes = Math.floor(Number(curTime) / 60);
+    let seconds = Number(curTime) % 60;
+    if (String(seconds).length < 2) seconds = "0" + String(seconds);
 
-return(
-    <>
-    <h1>STOPWATCH</h1>
-    <div>Time: {format()}</div>
-    <div>
-        <button className="button" onClick={handle}>{toggle}</button>
-        <button className="button" onClick={handlereset}>Reset</button>
+    return `${minutes}:${seconds}`;
+  };
+
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <div className="time">Time: {formatted()}</div>
+      <div>
+        <button type="button" onClick={handleWatch}>
+          {toggle}
+        </button>
+        <button type="button" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
     </div>
-    </>
-)
-
-
-}
-
+  );
+};
 
 export default Stopwatch;
